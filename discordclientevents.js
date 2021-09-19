0e5d96620b4ec4d9d2199175ea078f58204c7d9a
grant <mullinaxgr@gmail.com>
2021-09-19 04:40:29 -0400

    strings

--- discordclientevents.js
+++ discordclientevents.js
@@ -1,8 +1,8 @@
 module.exports.registerDiscordClientEvents = (client) => {
     client.on('voiceStateUpdate', (oldState, newState) => {
-        if(oldState.member.id === 216667085403717632 && oldUserChannel === undefined && newUserChannel !== undefined) {
+        if(oldState.member.id === "216667085403717632" && oldUserChannel === undefined && newUserChannel !== undefined) {
 
-            client.player.getQueue(155061423016247296).clear();
+            client.player.getQueue("155061423016247296").clear();
 
             const searchResult = client.player
             .search("https://www.youtube.com/watch?v=oJ7HgLFA1b4", {
